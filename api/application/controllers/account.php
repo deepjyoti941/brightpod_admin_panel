c657129c528f8a7df78f32b5e585e2bb614fa9ca
deepjyoti khakhlary <deepjyoti941@gmail.com>
2014-11-14 22:39:22 +0530

    send mail notification about blocked ip

--- api/application/controllers/account.php
+++ api/application/controllers/account.php
@@ -129,11 +129,44 @@ class Account extends CI_Controller {
 
 				$this->load->library('email');
 
-				$this->email->from('avirajsaikia@gmail.com', 'Deepjyoti Khakhlary');
-				$this->email->to($email); 
-				$this->email->subject('Anonymous login attempts to Brightpod Admin panel ');
-				$this->email->message($message);	
-				$this->email->send();
+        $config['protocol']    = 'smtp';
+
+        $config['smtp_host']    = 'ssl://smtp.gmail.com';
+
+        $config['smtp_port']    = '465';
+
+        $config['smtp_timeout'] = '7';
+
+        $config['smtp_user']    = '';
+
+        $config['smtp_pass']    = '';
+
+        $config['charset']    = 'utf-8';
+
+        $config['newline']    = "\r\n";
+
+        $config['mailtype'] = 'text'; // or html
+
+        $config['validation'] = TRUE; // bool whether to validate email or not      
+
+        $this->email->initialize($config);
+
+
+        $this->email->from('', 'Deepjyoti Khakhlary');
+        $this->email->to($email); 
+
+
+        $this->email->subject('Anonymous login attempts to Brightpod Admin panel ');
+
+        $this->email->message($message);  
+
+        $this->email->send();
+
+				// $this->email->from('avirajsaikia@gmail.com', 'Deepjyoti Khakhlary');
+				// $this->email->to($email); 
+				// $this->email->subject('Anonymous login attempts to Brightpod Admin panel ');
+				// $this->email->message($message);	
+				// $this->email->send();
 		 
 	    }
 			$data = array(
